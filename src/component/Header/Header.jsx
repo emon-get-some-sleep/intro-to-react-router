493e128aa1825f8b171e5aa36468eafd83d03b73
emonh0que <you@example.com>
2023-04-02 21:32:31 +0600

    learned nested route

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-           <a href="/home">Home</a>
+           <a href="/">Home</a>
            <a href="/about">About</a>
            <a href="/contact">Contact</a>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -9,25 +9,44 @@ import {
 import About from './component/About/About';
 import Contact from './component/Contact/Contact';
 import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App></App>,
+//     errorElement: <div>sorry not found</div>
+//   },
+//   {
+//     path: "/about",
+//     element: <About />
+//   },
+//   {
+//     path: "/contact",
+//     element: <Contact />
+//   }
+// ])
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App></App>,
-    errorElement: <div>sorry not found</div>
-  },
-  {
-    path: "/about",
-    element: <About />
-  },
-  {
-    path: "/contact",
-    element: <Contact />
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      },
+      
+    ]
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header></Header>
+   
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
