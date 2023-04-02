2af6c591c7a4e66efc4a720b1e728af975030cd6
emonh0que <you@example.com>
2023-04-02 23:16:34 +0600

    learned loader data in react router

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@ import About from './component/About/About';
 import Contact from './component/Contact/Contact';
 import Header from './component/Header/Header';
 import Home from './component/Home/Home';
+import First from './component/First/First';
+import Friends from './component/Friends/Friends';
 
 // const router = createBrowserRouter([
 //   {
@@ -33,8 +35,17 @@ const router = createBrowserRouter([
     element: <Home></Home>,
     children: [
       {
+        path: '/',
+        element: <First></First>
+      },
+      {
         path: '/about',
         element: <About></About>
+      },
+      {
+        path: 'friends',
+        element: <Friends></Friends>,
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users')
       },
       {
         path: '/contact',
